910c38be08e1e56494b2034bd2ce17459ec344ef
krivalex <new.krivalex@gmail.com>
2023-10-03 20:34:11 +0600

    update

--- src/Syntax/base.ts
+++ src/Syntax/base.ts
@@ -9,3 +9,16 @@ let tuple: [number, string] = [1, '1']
 let any: any = 1
 let void1: void = undefined
 let object1: { string: string; number: number } = { string: '1', number: 1 }
+
+function func1(number: number): null | number {
+  if (number > 10) {
+    return 1
+  } else if (number < 10) {
+    return 2
+  } else if (number === 10) {
+    return 3
+  }
+  return null
+}
+
+func1(1.7976931348623157e308 + 1)
